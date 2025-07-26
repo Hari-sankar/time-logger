1bfe1cc8646767680ed9c44621aea86965a4f4c1
Harisankar K S <harisankar.kalladathil@gmail.com>
2025-07-27 01:56:02 +0530

    Refactor websocket endpoint to improve user authentication handling

--- app/routes/chat_routes.py
+++ app/routes/chat_routes.py
@@ -1,17 +1,28 @@
+
+import uuid
+from app.utils.ws_auth import require_authenticated_user
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from app.services.chat_service import handle_chat
 
 router = APIRouter()
 
 @router.websocket("/ws")
-async def websocket_endpoint(
-    websocket: WebSocket
-):
+async def websocket_endpoint(websocket: WebSocket):
+    user_id = await require_authenticated_user(websocket)
+
+    if not user_id:
+        return  
     await websocket.accept()
+    thread_id = uuid.uuid4().hex  
     try:
         while True:
             data = await websocket.receive_text()
-            async for chunk in handle_chat(data):
-                await websocket.send_text(chunk)
+            response = await handle_chat(data, user_id=user_id,thread_id=thread_id)
+            if response == "__DISCONNECT__":
+                await websocket.send_text("👋 You have been logged out.")
+                await websocket.close()
+                return
+            await websocket.send_text(response)
+
     except WebSocketDisconnect:
-        print(f"Client disconnected")
+        print(f"WebSocket disconnected for user {user_id}")
